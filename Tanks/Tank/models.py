f15c93a718463ef126b9c01929f9391bba202296
Davidb2303 <dballesteros2303@gmail.com>
2025-03-17 00:48:37 -0500

    modificate TankModel

--- Tanks/Tank/models.py
+++ Tanks/Tank/models.py
@@ -4,8 +4,8 @@ from User.models import Usuario
 # Create your models here.
 class Tanque(models.Model):
     nombre = models.CharField(max_length=100)
-    capacidad_total = models.FloatField()
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="tanques")
+    capacidad = models.FloatField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="tanques", null=True)
 
     def __str__(self):
         return self.nombre
