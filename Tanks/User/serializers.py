1f90b77aca01f21b22270b45a95d6246471478f1
Davidb2303 <dballesteros2303@gmail.com>
2025-04-17 23:01:36 -0500

    Module Users

--- Tanks/User/serializers.py
+++ Tanks/User/serializers.py
@@ -1,8 +1,7 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Usuario
 
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
-        fields = ['id','username','email']
+        fields = ['id','username','email','rol']
--- Tanks/User/views.py
+++ Tanks/User/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Usuario
+from .serializers import UsuarioSerializer
+from .permissions import IsAdmin
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+    permission_classes = [IsAuthenticated, IsAdmin]
